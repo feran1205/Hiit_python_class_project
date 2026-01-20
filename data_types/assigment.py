d9734a74dacf4b361cafbeb808f0ec972c6f85a7
feran1205 <wendyfaloore@gmail.com>
2026-01-20 22:02:47 +0000

    refactor or optimize my assignment code file for better performance

--- data_types/assigment.py
+++ data_types/assigment.py
@@ -1,15 +1,17 @@
 var_1, var_2 ,var_3, VAR_X = 16, 17, 3, 5
-print(16/3)
-if (16/3) == 0:
+# print(16/3)
+
+answer = var_1 % var_3
+if answer == 0:
     print("Oh I got zero as a remainder")
 else:
     print("Real number")
 
-
-if (17/3) >= VAR_X:
-    print("Constant is greater than the result")
+result = var_2 / var_3
+if result >= VAR_X:
+    print("Constant is less than the result")
 else:
-    print("Constant is just less")
+    print("Constant is greater than the result")
 
 
 
