195dca989aa807115aee4fefaf250febb392aa94
feran1205 <wendyfaloore@gmail.com>
2026-01-23 20:20:10 +0000

    add basic tuple lessons and operations

---
+++ data_types/tuple_lesson.py
@@ -0,0 +1,20 @@
+my_tuple = [1, "hello"]
+print(type(my_tuple))
+my_tuple = tuple(my_tuple)
+print(type(my_tuple))
+
+
+
+
+
+
+
+student_tuple = ('Amanda', [98, 85, 87], 198)
+# print(type(student_tuple))
+first_name, grades, total = student_tuple 
+# print(first_name)
+# print(grades)
+
+
+update_value = 900
+student_tuple[2]
